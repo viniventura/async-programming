a53489db8f3245eeb55cc40cfcff46a62df71bd9
vsventura <vsventura@firjan.com.br>
2020-05-07 16:19:34 -0300

    Exemplo programação assíncrona com C#

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Síncrono
+            string UolContentSync = Service.GetPageUol();
+            Console.Write($"--- PÁGINA DA UOL----\n{UolContentSync}");
+
+            //Assíncrono - Método 1, 2 threads executando em paralelo
+            var taskUol = Service.GetPageUolAsync();
+            var taskTerra = Service.GetPageTerraAsync();
+
+            // DO THINGS....
+
+            // AGUARDO AS 2 TASKS FINALIZAREM
+            await Task.WhenAll(taskUol, taskTerra);
+
+            //OBTENDO OS RESULTADOS
+            string uolContent = await Service.GetPageUolAsync();
+            string terraContent = await Service.GetPageTerraAsync();
+
+            Console.Write($"--- PÁGINA DA UOL ----\n{uolContent}\n\n" +
+                $"--- PÁGINA DO TERRA ----\n{terraContent}");
+
+            //Assíncrono - Método 2, 2 threads executando em paralelo
+            Parallel.Invoke(() => Service.GetPageUol(), () => Service.GetPageUol());
+            // nesse momento as 2 funções acima já retornaram (finalizaram)
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Service.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public static class Service
+    {
+
+        public static string GetPageUol()
+        {
+            HttpClient httpClient = new HttpClient();
+            var httpResponseMessage = httpClient.GetAsync("https://www.uol.com.br/").Result;
+            return httpResponseMessage.Content.ReadAsStringAsync().Result;
+        }
+
+        public static async Task<string> GetPageUolAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+            var httpResponseMessage = await httpClient.GetAsync("https://www.uol.com.br/");
+            return await httpResponseMessage.Content.ReadAsStringAsync();
+        }
+
+        public static async Task<string> GetPageTerraAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+            var httpResponseMessage = await httpClient.GetAsync("https://www.terra.com.br/");
+            return await httpResponseMessage.Content.ReadAsStringAsync();
+        }
+    }
+}
